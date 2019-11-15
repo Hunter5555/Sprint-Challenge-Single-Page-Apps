b6d4e8164a33bc5cbe3d696088350bc4c5bb83bc
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-15 10:45:34 -0700

    almost mvp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+  
 
+const HeaderStyle = styled.div`
+display: flex;
+justify-content: center;
+background-color:skyblue;
+color:white;
+`
 export default function Header() {
   return (
+    <HeaderStyle>
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
     </header>
+    </HeaderStyle>
   );
-}
+} 
